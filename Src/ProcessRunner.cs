04a922ab47bcd7273f3bc7dde4a1bde5cca3cc77
UCHIYAMA Ryota <lessironglance@gmail.com>
2022-10-22 09:31:33 +0900

    init

---
+++ Src/ProcessRunner.cs
@@ -0,0 +1,48 @@
+﻿namespace Medoz.Diagnostics;
+
+using System.Diagnostics;
+
+
+public static class ProcessRunner
+{
+
+    public static void RunAsync(string fileName, string? arguments, string? workingDirectory = null, IDictionary<string, string>? environmentVariable = null)
+    {
+
+    }
+
+    public static void Run(string fileName, string? arguments )
+    {
+        ProcessStartInfo psi = new()
+        {
+            FileName = fileName,
+            Arguments = arguments,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            ErrorDialog = false,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+        };
+
+        Process p = new()
+        {
+            StartInfo = psi,
+            EnableRaisingEvents = true,
+        }
+        
+        p.OutputDataReceived += (sender, e) =>
+        {
+
+        };
+
+        if (!process.Start())
+        {
+            throw new Exception("");
+        }
+
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+    }
+
+
+}
